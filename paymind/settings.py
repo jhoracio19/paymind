ecb7ae18db290dda2c7897c2ab9743f9df2f1a09
Jose Horacio <jhoracio19@hotmail.com>
2025-11-16 17:07:44 -0600

    chore(ui): improve navigation label and spanish date formatting

--- paymind/settings.py
+++ paymind/settings.py
@@ -108,13 +108,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-mx'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+USE_L10N = True
 
 
 # Static files (CSS, JavaScript, Images)
